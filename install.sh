d3421e87e327fb54aa07e788486ace5e80a7b8c6
WeCan Group <mutalebe@live.com>
2018-07-08 00:18:30 +0430

    Initial commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+# cat /etc/system-release | grep -o '.*\.'
+OS=$( cat /etc/system-release | cut -d ' ' -f 1)
+VER=$( cat /etc/system-release | cut -d ' ' -f 3)
+
+if [ $VER < 7 ]
+then
+   cp -fr Makefile.7.x Makefile
+else
+  cp -fr Makefile.6.x Makefile
+fi
+
+make
