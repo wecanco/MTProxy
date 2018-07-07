8fe93f5c91494c8f304523c14a950f761cab8120
WeCan Group <mutalebe@live.com>
2018-07-08 00:50:53 +0430

    fix

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ VER=$( cat /etc/system-release | cut -d ' ' -f 3)
 
 if [ $VER < 7 ]
 then
-   cp -fr Makefile.7.x Makefile
+   cp -fr Makefile.6.x Makefile
 else
-  cp -fr Makefile.6.x Makefile
+  cp -fr Makefile.7.x Makefile
 fi
 
 make
