c20ea4dbdc5ca872de1cd7c8ae8b3d3da03e40aa
WeCan Group <mutalebe@live.com>
2018-07-08 01:01:46 +0430

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 # cat /etc/system-release | grep -o '.*\.'
 OS=$( cat /etc/system-release | cut -d ' ' -f 1)
 VER=$( cat /etc/system-release | cut -d ' ' -f 3)
-
+echo "OS Version: $VER \n"
 if [ $VER < 7 ]
 then
    cp -fr Makefile.6.x Makefile
@@ -11,4 +10,9 @@ else
   cp -fr Makefile.7.x Makefile
 fi
 
-make
+make all
+cd objs/bin
+curl -s https://core.telegram.org/getProxySecret -o proxy-secret
+curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
+adduser -M mtproxy
+
